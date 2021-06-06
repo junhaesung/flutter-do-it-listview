d9f62368d4aa0258ca31883920de10f624676715
Haeseong Jeon <seong0428@gmail.com>
2021-06-06 14:31:27 +0900

    4단계: FirstApp, SecondApp 클래스에서 animalList 데이터를 받아올 수 있도록 수정

--- lib/sub/firstPage.dart
+++ lib/sub/firstPage.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../animalItem.dart';
+
 class FirstApp extends StatelessWidget {
+  final List<Animal> list;
+
+  FirstApp({required Key key, required this.list}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text('첫 번째 페이지')));
--- lib/sub/secondPage.dart
+++ lib/sub/secondPage.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../animalItem.dart';
+
 class SecondApp extends StatelessWidget {
+  final List<Animal> list;
+
+  SecondApp({required Key key, required this.list}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text('두 번째 페이지')));
