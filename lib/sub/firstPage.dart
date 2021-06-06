53c59b776fa55bd3d0c49bbae58792f35aee2e3c
Haeseong Jeon <seong0428@gmail.com>
2021-06-06 14:02:39 +0900

    1단계: 페이지 및 이미지 파일 추가

---
+++ lib/sub/firstPage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('첫 번째 페이지')));
+  }
+}
---
+++ lib/sub/secondPage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SecondApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('두 번째 페이지')));
+  }
+}
