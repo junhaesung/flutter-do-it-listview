4f77bb66a179ab7b06427dda7b0ec90acac44aca
Haeseong Jeon <seong0428@gmail.com>
2021-06-06 14:27:58 +0900

    3단계: Animal 객체 생성하고 animalList 에 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'animalItem.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -22,92 +24,79 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+  List<Animal> animalList = List.empty(growable: true);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+
+    animalList.add(Animal(
+      animalName: '벌',
+      kind: '곤충',
+      imagePath: 'images/bee.png',
+    ));
+    animalList.add(Animal(
+      animalName: '고양이',
+      kind: '포유류',
+      imagePath: 'images/cat.png',
+    ));
+    animalList.add(Animal(
+      animalName: '젖소',
+      kind: '포유류',
+      imagePath: 'images/cow.png',
+    ));
+    animalList.add(Animal(
+      animalName: '강아지',
+      kind: '포유류',
+      imagePath: 'images/dog.png',
+    ));
+    animalList.add(Animal(
+      animalName: '여우',
+      kind: '포유류',
+      imagePath: 'images/fox.png',
+    ));
+    animalList.add(Animal(
+      animalName: '원숭이',
+      kind: '영장류',
+      imagePath: 'images/monkey.png',
+    ));
+    animalList.add(Animal(
+      animalName: '돼지',
+      kind: '포유류',
+      imagePath: 'images/pig.png',
+    ));
+    animalList.add(Animal(
+      animalName: '늑대',
+      kind: '포유류',
+      imagePath: 'images/wolf.png',
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter Demo Home Page'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(),
     );
   }
 }
